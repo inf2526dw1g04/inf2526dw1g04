d031938584f901fd2a7c3c8298db450094111868
Mauro Marques <rmauromarques@gmail.com>
2026-01-21 16:58:53 +0000

    mais entulho

--- Parte2/index.js
+++ Parte2/index.js
@@ -2,20 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./src/swagger');
+const pacientesRoutes = require('./src/routes/pacientes.routes');
+const medicosRoutes = require('./src/routes/medicos.routes');
+const especialidadesRoutes = require('./src/routes/especialidades.routes');
+const consultasRoutes = require('./src/routes/consultas.routes');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/consultas', (req, res) => {
-  res.json({ mensagem: 'Lista de consultas' });
+// Rotas
+app.use('/pacientes', pacientesRoutes);
+app.use('/medicos', medicosRoutes);
+app.use('/especialidades', especialidadesRoutes);
+app.use('/consultas', consultasRoutes);
+
+
+// Rota raiz
+app.get('/', (req, res) => {
+  res.json({ mensagem: 'API 100%' });
 });
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
+  console.log(`API na porta ${PORT}`);
   console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
 });
--- Parte2/src/database/db.js
+++ Parte2/src/database/db.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-require('dotenv').config({ path: __dirname + '/../../.env' }); // sobe 2 pastas até raiz
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      // localhost
-  user: process.env.DB_USER,      // root
-  password: process.env.DB_PASSWORD, // root
-  database: process.env.DB_NAME   // clinicamedica
+  host: process.env.DB_HOST,      
+  user: process.env.DB_USER,      
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
   if (err) {
-    console.error('ERRO - Base de dados', err);
+    console.error('ERRO - Base de dados:', err.message);
   } else {
     console.log('Base de dados - ON!');
   }
